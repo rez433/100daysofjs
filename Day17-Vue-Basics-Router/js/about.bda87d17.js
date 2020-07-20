9b70c3c070258e154bf02a033a1f157666be901d
Reza Rezaee <rezaee86@outlook.com>
2020-07-20 09:12:22 +0200

    Day17- TodoList App with Vue Basics and Vue Router

---
+++ Day17-Vue-Basics-Router/js/about.bda87d17.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,u){"use strict";u.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},n=[function(){var e=this,t=e.$createElement,u=e._self._c||t;return u("div",{staticClass:"about"},[u("h1",[e._v("Day 17 - Vue Basics and Vue Router")]),u("p",[e._v("It is a TodoList app made using Vue JS with Vue Router")])])}],s=u("2877"),i={},o=Object(s["a"])(i,a,n,!1,null,null,null);t["default"]=o.exports}}]);
+//# sourceMappingURL=about.bda87d17.js.map
